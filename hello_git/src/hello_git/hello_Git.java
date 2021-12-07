a71b4b999c70845567815e2fd7dc0fba136e481f
Thomas Jonatas Espindola dos Anjos <60404390+thomasdosanjos@users.noreply.github.com>
2021-12-06 21:25:25 -0300

    Adicionei arquivos com o Git

---
+++ hello_git/src/hello_git/hello_Git.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package hello_git;
+
+/**
+ * @author thomas Dos anjos
+ * exemplo de integração da IDE eclipse com o Git
+ */
+public class hello_Git {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Git");
+		
+		
+
+	}
+
+}
